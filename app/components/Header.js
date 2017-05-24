d8d30460f85d9a9e15447db73e3f940669e29be9
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-24 11:11:53 +0100

    header positioning in view

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Header = () => {
+  const { viewStyle } = styles;
   return (
-    <Text>My Items</Text>
+    <View style={viewStyle}>
+      <Text>MY ITEMS</Text>
+    </View>
   );
 };
+
+const styles = {
+  viewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
+    paddingTop: 15,
+    // backgroundColor: '#DEDEDE',
+    borderBottom: 1,
+    borderBottomWidth: 0.5
+
+  }
+}
 
 export default Header;
