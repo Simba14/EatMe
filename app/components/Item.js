fedb7da90387fc0fc929aeee5440e4aa746f37cd
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-25 12:19:24 +0100

    WIP set date to expire and style highlight

--- app/components/Item.js
+++ app/components/Item.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React, { Component } from 'react';
 import {
   View,
@@ -9,23 +10,32 @@ class Item extends Component {
   state = {};
 
   render () {
+
+    if (this.daysToExpire() < 3) {
+      var style = styles.shortToExpire;
+    } else if (this.daysToExpire() < 7) {
+      var style = styles.mediumToExpire;
+    } else {
+      // var style = styles.viewStyle;
+    }
+
     return(
       <View>
-        <View style={styles.viewStyle}>
+        <View style={[styles.viewStyle, style]}>
           <Text style={styles.textStyleName}>Avocado from Sainsburys</Text>
-          <Text style={styles.textStyleDate}>Expires on: 27/05/17</Text>
-        </View>
-        <View style={styles.viewStyle}>
-          <Text style={styles.textStyleName}>Banana from Tesco</Text>
-          <Text style={styles.textStyleDate}>Expires on: 27/05/17</Text>
-        </View>
-        <View style={styles.viewStyle}>
-          <Text style={styles.textStyleName}>Coconut from Thailand</Text>
-          <Text style={styles.textStyleDate}>Expires on: 27/05/17</Text>
+          <Text style={styles.textStyleDate}>Expires in: {this.daysToExpire()} days</Text>
         </View>
       </View>
     )
   }
+  daysToExpire() {
+    var today = moment().format('l');
+    var expires = moment('27-5-2017', 'DD-M-YYYY');
+    return expires.diff(today,'days');
+    // if expires in 0 days return string today
+  }
+
+
 }
 
 const styles = StyleSheet.create({
@@ -35,7 +45,18 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 20,
     marginTop: 20,
-    height: 70
+    height: 70,
+    borderRadius: 2,
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1
+  },
+  mediumToExpire: {
+    backgroundColor: '#F7B767',
+  },
+  shortToExpire: {
+    backgroundColor: '#F1BABA',
   },
   textStyleName: {
     marginLeft: 10,
