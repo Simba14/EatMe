db176bb2891bad7109edf920b0906328bfdbc9f3
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-26 14:46:32 +0100

    Sorted by expiration date

--- app/components/ListView.js
+++ app/components/ListView.js
@@ -10,7 +10,7 @@ import { ItemDB } from './Schema';
 
 class ListView extends Component {
   getAllItems() {
-    let results = [ realm.objects('ItemDB')];
+    let results = [ realm.objects('ItemDB').sorted('expirationDate')];
     itemObject = results.map(x => Object.assign({}, x));
     itemArray = Object.values(itemObject[0]);
     return this.renderItems(itemArray);
