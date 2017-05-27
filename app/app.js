af56e8df2803d7e9e357d860d84657f0efcb091e
Simba14 <seanblundell@gmail.com>
2017-05-27 13:56:37 +0100

    Change App from const to class

--- app/app.js
+++ app/app.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux'; //communication glue between react and redux
 import { createStore } from 'redux';
 import reducers from './reducers';
 import Router from './navigations/Router.js'
 
-import AddItemScreen from './screens/AddItemScreen';
-import MyItemsScreen from './screens/MyItemsScreen';
+export default class App extends Component {
 
-const App = () => {
-
-  componentDidMount() {
-    AppState.addEventListener('change', this.handleAppStateChange);
-  }
-
-  componentWillMount() {
-    AppState.removeEventListener('change', this.handleAppStateChange);
-  }
-
-  handleAppStateChange(appState) {
-    if (appState === 'background') {
-      console.log('app is in background')
-    }
-  }
-
-  return (
+  // componentDidMount() {
+  //   AppState.addEventListener('change', this.handleAppStateChange);
+  // }
+  //
+  // componentWillMount() {
+  //   AppState.removeEventListener('change', this.handleAppStateChange);
+  // }
+  //
+  // handleAppStateChange(appState) {
+  //   if (appState === 'background') {
+  //     console.log('app is in background')
+  //   }
+  // }
+  render() {
+    return (
     // <Provider store={createStore}> // provider works with the store. translates the data in the store to be used by react
       <Router />
     // </Provider>
-  );
+    );
+  }
 }
-
-export default App;
