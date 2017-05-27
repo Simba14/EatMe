488f426614f9f3b3a1d2d552a84613f89953e33c
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-27 14:34:12 +0100

    camera now shows a live preview of the rear camera

--- app/components/BarcodeScanner.js
+++ app/components/BarcodeScanner.js
@@ -3,39 +3,39 @@ import { View, StyleSheet, TouchableHighlight, Text } from 'react-native';
 import Camera from 'react-native-camera';
 
 class BarcodeScanner extends Component {
-
-  clickedMe() {
-    alert("was touched");
-  }
-
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Camera
           ref={(cam) => {
             this.camera = cam;
           }}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture}>[CAPTURE]</Text>
         </Camera>
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center'
   },
-  cameraButton: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'pink'
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
   }
-})
+});
 
 export default BarcodeScanner;
