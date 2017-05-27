c93fa94b1ea0e71adb5c72b201d1dc5601d2094a
Simba14 <seanblundell@gmail.com>
2017-05-27 13:34:27 +0100

    Initial set up

--- app/app.js
+++ app/app.js
@@ -9,6 +9,21 @@ import AddItemScreen from './screens/AddItemScreen';
 import MyItemsScreen from './screens/MyItemsScreen';
 
 const App = () => {
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillMount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  }
+
+  handleAppStateChange(appState) {
+    if (appState === 'background') {
+      console.log('app is in background')
+    }
+  }
+
   return (
     // <Provider store={createStore}> // provider works with the store. translates the data in the store to be used by react
       <Router />
