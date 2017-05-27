76d8f0a0264314583c2889e170a592176999dcc9
Magnus Holm <mail@magnusholm.com>
2017-05-27 22:36:01 +0100

    Style camera preview in BarcodeScanner to sit on top

--- app/components/BarcodeScanner.js
+++ app/components/BarcodeScanner.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { View, StyleSheet, TouchableHighlight, Text, Alert } from 'react-native';
 import Camera from 'react-native-camera';
 import axios from 'axios';
+import _ from 'lodash';
 
 class BarcodeScanner extends Component {
   constructor() {
     super();
     this.state = {
-      // showCamera: true,
-      scannedItem: "loading..."
+      showCamera: true,
+      scannedItem: ""
     }
-    this.onBarCodeRead = _.throttle(this.onBarCodeRead, 500);
   }
 
   render() {
@@ -38,7 +38,7 @@ class BarcodeScanner extends Component {
   }
 
   requestItemInformation(data) {
-    // this.setState({showCamera: false});
+    this.setState({showCamera: false});
     var barcodeNumber = data.data;
     axios.get('https://api.upcdatabase.org/json/128009a43963c119609bd223c9f249cf/' + barcodeNumber)
       .then(response => this.setState({ scannedItem: response.data.description }));
@@ -48,12 +48,14 @@ class BarcodeScanner extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   preview: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: -30,
+    marginBottom: 300,
   },
   capture: {
     flex: 0,
