af4f27b1ac32bac008262947996ae544a8b99fe4
Simba14 <seanblundell@gmail.com>
2017-05-28 13:40:22 +0100

    Incorporate pluralize method in message

--- app/app.js
+++ app/app.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { AppState, Platform } from 'react-native';
-import { Provider } from 'react-redux'; //communication glue between react and redux
-import { createStore } from 'redux';
-import reducers from './reducers';
+// import { Provider } from 'react-redux'; //communication glue between react and redux
+// import { createStore } from 'redux';
+// import reducers from './reducers';
 import Router from './navigations/Router.js'
 import PushNotification from 'react-native-push-notification'
+import Realm from 'realm';
+import { realm } from './components/Schema';
+import { ItemDB } from './components/Schema';
+import moment from 'moment';
+import pluralize from 'pluralize';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.handleAppStateChange = this.handleAppStateChange.bind(this);
+    this.state = {
+      expiringItemsCount: this.getExpiringItemsCount()
+    }
   }
 
   componentDidMount() {
@@ -23,12 +31,23 @@ export default class App extends Component {
   handleAppStateChange(appState) {
     if (appState === 'background') {
       let date = new Date(Date.now() + (2 * 1000));
+      let message = 'You have an ' + pluralize('item',2) + ' that will expire today';
+      console.log(message);
 
       PushNotification.localNotificationSchedule({
-        message: "Your Food Is Going To TERMINATE!",
+        title: 'Your Food Is Going To TERMINATE!',
+        message: message, //+ this.getExiringItemsCount() + 'items that are set to expire',
         date,
       });
+
     }
+  }
+
+  getExpiringItemsCount() {
+    let results = [ realm.objects('ItemDB').sorted('expirationDate')];
+    itemObject = results.map(x => Object.assign({}, x));
+    itemArray = Object.values(itemObject[0]);
+    return itemArray.length;
   }
 
   render() {
--- app/screens/MyItemsScreen.js
+++ app/screens/MyItemsScreen.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import { SubmitButton, ViewContainer, UpperContainer, LowerContainer } from '../components/common';
 import ListView from '../components/ListView';
-import { realm } from '../components/Schema';
-import ItemDB from '../components/Schema';
 import PushController from '../components/PushController'
 
 class MyItemsScreen extends Component {
