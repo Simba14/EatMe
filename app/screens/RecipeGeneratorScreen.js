69f0fc67e4b3118f68cb8c8f50d37f747fa8f93c
Magnus Holm <mail@magnusholm.com>
2017-05-28 13:41:59 +0100

    Create RecipeGeneratorScreen component

---
+++ app/screens/RecipeGeneratorScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class RecipeGeneratorScreen extends Component {
+
+  render() {
+
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    )
+
+
+  }
+
+
+}
+
+export default RecipeGeneratorScreen;
