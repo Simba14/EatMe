1b865ad5e26b24b760015ce0c515381cd03148ea
Magnus Holm <mail@magnusholm.com>
2017-05-28 18:48:06 +0100

    WIP: Trying to get aroud ios https protocol enforcement

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -1,33 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import Realm from 'realm';
 import { realm } from './Schema';
 import { ItemDB } from './Schema';
+import axios from 'axios';
 
 
 class RecipeList extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: [] };
+    this.state = {
+      items: ["Potatoes"],
+      recipe: []
+    };
 
-  }
-
-  getFoodItems() {
-    let results = [ realm.objects('ItemDB').sorted('expirationDate')];
-    itemObject = results.map(x => Object.assign({}, x));
-    itemArray = Object.values(itemObject[0]);
-    this.setState({ items: itemArray })
   }
 
   componentWillMount() {
-    this.getFoodItems();
+    // var item1 = this.state.items[0];
+    axios.get('http://recipepuppy.com/api/?i=' + 'potato')
+      .then(response => this.setState({ recipe: response.data.results[0].title }));
+      .catch(error => Alert.alert('no response...'));
   }
+
+  axios.get('/user?ID=12345')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  // getFoodItems() {
+  //   let results = [ realm.objects('ItemDB').sorted('expirationDate')];
+  //   itemObject = results.map(x => Object.assign({}, x));
+  //   itemArray = Object.values(itemObject[0]);
+  //   this.setState({ items: itemArray })
+  // }
+  //
+  // componentWillMount() {
+  //   this.getFoodItems();
+  // }
+
+  // renderRecipe() {
+  //   return this.state.recipe;
+  // }
 
   render() {
 
     return (
       <View>
-        <Text> Recipes! {this.state.items[0].itemName} </Text>
+        {this.state.recipe[0]}
       </View>
     );
   };
