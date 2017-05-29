e3abc710af9248bb09545fd24cff5f619a4b704f
Magnus Holm <mail@magnusholm.com>
2017-05-29 16:03:35 +0100

    Style recipe and recipelist components

--- app/components/Recipe.js
+++ app/components/Recipe.js
@@ -6,8 +6,13 @@ class Recipe extends Component {
   render() {
     return(
       <View style={styles.recipeViewStyle}>
-        <Image source={{ uri: this.props.recipe.image_url}} style={styles.imageStyle} />
-        <Text style={styles.textStyle}> {this.props.recipe.title}</Text>
+        <View style={styles.imageViewStyle}>
+          <Image source={{ uri: this.props.recipe.image_url}} style={styles.imageStyle} />
+        </View>
+        <View style={styles.titleViewStyle}>
+          <Text style={styles.titleStyle}> {this.props.recipe.title}</Text>
+          <Text style={styles.publisherStyle}> Publisher: {this.props.recipe.publisher} </Text>
+        </View>
       </View>
     );
   };
@@ -17,22 +22,37 @@ const styles = {
   recipeViewStyle: {
     borderWidth: 0.5,
     borderColor: '#DEDEDE',
-    height: 150,
+    height: 170,
     borderRadius: 2,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1
+    shadowOpacity: 0.1,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20
   },
-  textStyle: {
-    marginLeft: 10,
-    marginTop: 10
+  imageViewStyle: {
+    height: 100,
   },
   imageStyle: {
+    opacity: 0.9,
     height: 60,
     width: 30,
     flex: 1,
     width: null
-  }
+  },
+  titleViewStyle: {
+    height: 70,
+  },
+  titleStyle: {
+    marginLeft: 10,
+    marginTop: 10
+  },
+  publisherStyle: {
+    marginLeft: 10,
+    marginTop: 10,
+    // fontStyle: 'italic'
+  },
 }
 
 export default Recipe;
--- app/screens/RecipeGeneratorScreen.js
+++ app/screens/RecipeGeneratorScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { ViewContainer, UpperContainer } from '../components/common';
+import { ViewContainer, UpperContainer, LowerContainer } from '../components/common';
 import RecipeList from '../components/RecipeList';
 
 class RecipeGeneratorScreen extends Component {
@@ -11,6 +11,8 @@ class RecipeGeneratorScreen extends Component {
         <UpperContainer>
           <RecipeList  />
         </UpperContainer>
+        <LowerContainer>
+        </LowerContainer>
       </ViewContainer>
     );
   };
