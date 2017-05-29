1abf042c81c72e790fc5188a72f69405190a42b3
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-29 11:12:49 +0100

    padding inside the textContainer

--- app/components/Item.js
+++ app/components/Item.js
@@ -41,31 +41,30 @@ class Item extends Component {
 };
 
 const styles = StyleSheet.create( {
+  swipeStyle: {
+    backgroundColor: 'white'
+  },
   textContainer: {
     height: 70,
     backgroundColor: '#9EE2CC',
     flexDirection: 'column',
     justifyContent: 'space-around',
-    marginBottom: 1
+    marginBottom: 1,
+    padding: 7
   },
   mediumToExpire: {
-    backgroundColor: '#F5CC99',
+    backgroundColor: '#F5CC99'
   },
   shortToExpire: {
-    backgroundColor: '#F1BABA',
+    backgroundColor: '#F1BABA'
   },
   textStyleName: {
     fontSize: 15,
-    color: '#4A4A4A',
-    marginTop: 7
+    color: '#4A4A4A'
   },
   textStyleDate: {
     fontSize: 15,
-    color: '#4A4A4A',
-    marginBottom: 7
-  },
-  swipeStyle: {
-    backgroundColor: 'white',
+    color: '#4A4A4A'
   }
 });
 
