9312d4ce4045db690ab5f1ad564f1329991fd6d5
Magnus Holm <mail@magnusholm.com>
2017-05-29 15:31:41 +0100

    Style noRecipes screen

--- app/components/common/NoRecipes.js
+++ app/components/common/NoRecipes.js
@@ -4,13 +4,38 @@ import { UpperContainer } from './UpperContainer';
 
 const NoRecipes = () => {
   return (
-    <View>
+    <View style={styles.viewStyle}>
       <UpperContainer>
-        <Text>No recipes found </Text>
-        <Text>Add items blablabla</Text>
+        <Text style={styles.title}>No recipes found </Text>
+        <Text style={styles.body}>Add some items to your list before generating recipes</Text>
       </UpperContainer>
     </View>
   )
+}
+
+const styles = {
+  viewStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  title: {
+    marginTop: 90,
+    textAlign: 'center',
+    fontSize: 32,
+    fontFamily: 'System',
+    color: '#323232'
+  },
+  body: {
+    marginTop: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'System',
+    color: '#323232'
+  }
 }
 
 export default NoRecipes;
