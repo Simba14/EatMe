7143287e75bc9ab6d8ef9d2b05a9a6af4705bbd6
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-30 14:42:32 +0100

    still trying to get Barcode Scanner passing

--- __tests__/BarcodeScanner-test.js
+++ __tests__/BarcodeScanner-test.js
@@ -1,11 +1,11 @@
 import 'react-native';
 import React from 'react';
-import BarcodeScanner from '../app/components/BarcodeScanner';
+import ListView from '../app/components/Item';
 import renderer from 'react-test-renderer';
 
-test('renders the BarcodeScanner component correctly', () => {
+test('renders the ListView component correctly', () => {
   const tree = renderer.create(
-    <BarcodeScanner />
+    <ListView />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
